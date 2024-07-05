9ad39e1a9ceeb3158cb709b87aa124ba0f8085a4
Maxim Popov <itsmail4max@gmail.com>
2024-07-05 15:55:03 +0200

    added karlsruhe

--- constants.py
+++ constants.py
@@ -4,5 +4,5 @@ DATASET_DIR = Path("datasets")
 BUILDING_DATASET_DIR = DATASET_DIR / "building_footprints"
 SENTINEL_DATASET_DIR = DATASET_DIR / "sentinel"
 SAVE_DIR = DATASET_DIR / "training"
-CITIES = ["Bologna, Italy", "Milan, Italy", "Split, Croatia", "Valencia, Spain", "Oslo, Norway", "Krasnodar, Russia", "Paris, France", "Barcelona, Spain", "Berlin, Germany"]
+CITIES = ["Bologna, Italy", "Milan, Italy", "Split, Croatia", "Valencia, Spain", "Oslo, Norway", "Krasnodar, Russia", "Paris, France", "Barcelona, Spain", "Berlin, Germany", "Karlsruhe, Germany"]
 
