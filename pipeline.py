bf8ce3feef50118c10701077a80d35f7ac37755e
irmasuppes <irma.suppes@yandex.ru>
2024-07-07 03:02:17 +0200

    Add params saving

--- pipeline.py
+++ pipeline.py
@@ -2,20 +2,21 @@ from datetime import datetime
 import json
 import torch
 from torch.utils.data import DataLoader
-# from torchvision.models.segmentation import deeplabv3_mobilenet_v3_large, fcn_resnet101, fcn_resnet50
 from model import SegmentationModel, UNet
 from training import train, test
 from constants import SAVE_DIR, MODELS_DIR
-import segmentation_models_pytorch as smp
+import warnings
+warnings.filterwarnings("ignore")
 
 
-def load_data(path = SAVE_DIR, batch_size=32):
-    
-    train = torch.load(path / 'train.pt')
+def load_data(dataset_name,  batch_size=32):
+    print(f'Loading datasets {dataset_name}')
+
+    train = torch.load(SAVE_DIR / f'{dataset_name}_train.pt')
     train_loader = DataLoader(train, batch_size=batch_size, shuffle=True)
-    val = torch.load(path / 'val.pt')
+    val = torch.load(SAVE_DIR / f'{dataset_name}_val.pt')
     val_loader = DataLoader(val, batch_size=batch_size, shuffle=False)
-    test = torch.load(path / 'test.pt')
+    test = torch.load(SAVE_DIR / f'{dataset_name}_test.pt')
     test_loader = DataLoader(test, batch_size=batch_size, shuffle=False)
 
     return train_loader, val_loader, test_loader
@@ -24,7 +25,14 @@ def load_data(path = SAVE_DIR, batch_size=32):
 def run_pipeline():
     C = 4
     lr = 0.001
-    num_epochs = 20
+    num_epochs = 30
+    augmented = False
+    augmentation_type = 'rotate_reflect'
+    patch_size = 32
+    batch_size = 32
+
+    dataset_name = f'{str(patch_size)}' if not augmented else f'{str(patch_size)}_augmented_{augmentation_type}'
+
     if torch.cuda.is_available():
         device = torch.device('cuda')
     elif torch.backends.mps.is_available():
@@ -32,15 +40,15 @@ def run_pipeline():
     else:
         device = torch.device('cpu')    
 
-    train_loader, val_loader, test_loader = load_data()
+    train_loader, val_loader, test_loader = load_data(dataset_name)
 
-    # model = SegmentationModel(num_channels=C)
-    model = smp.Unet(
-        encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-        encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
-        in_channels=4,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-        classes=1,                      # model output channels (number of classes in your dataset)
-    )
+    model = SegmentationModel(num_channels=C)
+    # model = smp.Unet(
+    #     encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+    #     encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+    #     in_channels=4,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+    #     classes=1,                      # model output channels (number of classes in your dataset)
+    # )
 
     # model = UNet(n_class=1)
     model.to(device)
@@ -49,13 +57,17 @@ def run_pipeline():
 
     metrics = test(model, test_loader=test_loader, device=device, plot=False)
 
-    save(model, num_epochs, lr, C, metrics)
+    save(model, patch_size, augmented, augmentation_type, batch_size, num_epochs, lr, C, metrics)
 
 
-def save(model, epochs, lr, C, metrics):
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+def save(model, patch_size, augmented, augmentation_type, batch_size, epochs, lr, C, metrics):
+    timestamp = datetime.now().strftime('%d%m_%H%M')
 
     saved_params = {}
+    saved_params['patch_size'] = patch_size
+    saved_params['augmented'] = augmented
+    saved_params['augmentation_type'] = augmentation_type
+    saved_params['batch_size'] = batch_size
     saved_params['learning_rate'] = lr
     saved_params['number_of_epochs'] = epochs
     saved_params['number_of_channels'] = C
